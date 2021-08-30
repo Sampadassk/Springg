0ad90a73649d537154d6b69118ab47fc077aa4b7
Sampadassk <87821730+Sampadassk@users.noreply.github.com>
2021-08-30 18:58:00 +0530

    Add files via upload

---
+++ java/com/springcore/Student.java
@@ -0,0 +1,40 @@
+package com.springcore;
+
+public class Student {
+	private int Id;
+	private String studentName;
+	private String studentAddress;
+	public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+	public String getStudentName() {
+		return studentName;
+	}
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	public String getStudentAddress() {
+		return studentAddress;
+	}
+	public void setStudentAddress(String studentAddress) {
+		this.studentAddress = studentAddress;
+	}
+	public Student(int id, String studentName, String studentAddress) {
+		super();
+		Id = id;
+		this.studentName = studentName;
+		this.studentAddress = studentAddress;
+	}
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public String toString() {
+		return "Student [Id=" + Id + ", studentName=" + studentName + ", studentAddress=" + studentAddress + "]";
+	}
+
+}
