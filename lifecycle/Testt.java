71fc093c666b3a0eb47f9146abc98f68d4df4538
Sampadassk <87821730+Sampadassk@users.noreply.github.com>
2021-09-07 19:47:51 +0530

    Add files via upload

---
+++ lifecycle/Testt.java
@@ -0,0 +1,18 @@
+package com.springcore.lifecycle;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Testt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AbstractApplicationContext context= new ClassPathXmlApplicationContext("com/springcore/lifecycle/config.xml");
+		context.registerShutdownHook();
+
+		Example example=(Example) context.getBean("example");
+		System.out.println(example);
+	}
+
+}
